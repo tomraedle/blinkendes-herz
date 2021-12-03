e599df7ab8385f8239d3f8ab0f28a2e379b780ba
tomraedle <tomraedle@gmx.de>
2021-12-03 22:42:18 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,5 @@
+for (let index = 0; index < 6; index++) {
+    music.playMelody("C D E F G G A A ", 120)
+    music.playMelody("G A A A A G F F ", 120)
+    music.playMelody("F F E E D D D C ", 120)
+}
